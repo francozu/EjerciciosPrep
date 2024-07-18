11248e16b873f9efd33b5afe81003834520317d9
Franco <franko_zufia@hotmail.com>
2024-07-17 23:58:59 -0300

    till excercise 8 done

--- C6 Bucles/ejercicios/07.js
+++ C6 Bucles/ejercicios/07.js
@@ -3,6 +3,11 @@ function esParYDivisiblePorTres(a) {
   // Retorna true si este es par y divisible por tres a la vez.
   // Retorna false si no lo es.
   // Tu código:
+  if(a % 2 ==0 && a % 3 ==0)console.log(true);
+    else console.log(false);
 }
 
+esParYDivisiblePorTres(6);
+esParYDivisiblePorTres(618);
+esParYDivisiblePorTres(15);
 module.exports = esParYDivisiblePorTres;
--- C6 Bucles/ejercicios/08.js
+++ C6 Bucles/ejercicios/08.js
@@ -3,6 +3,12 @@ function esPositivoOInferiorA10(a) {
    // Retorna true si es positivo y menor que 10.
    // Retorna false en caso contrario.
    // Tu código:
+   if (a > 0 && a < 10)console.log(true);
+   else console.log(false);
 }
+esPositivoOInferiorA10(3);
+esPositivoOInferiorA10(10);
+esPositivoOInferiorA10(-6);
+esPositivoOInferiorA10(14);
 
 module.exports = esPositivoOInferiorA10;
